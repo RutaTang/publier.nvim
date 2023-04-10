caefa0c37fd824ad57db249aff7399affed1c079
RutaTang <rutatangpersonal@gmail.com>
2023-04-10 13:23:04 +0800

    init

--- lua/publier/init.lua
+++ lua/publier/init.lua
@@ -1,9 +1,15 @@
-print(1)
-print(1)
-print(1)
-print(1)
-print(1)
-print(2)
-print(3)
-print(3)
-print(1)
+vim.cmd[[echo 1]]
+vim.cmd[[echo 1]]
+vim.cmd[[echo 1]]
+vim.cmd[[echo 1]]
+vim.cmd[[echo 1]]
+vim.cmd[[echo 1]]
+vim.cmd[[echo 1]]
+vim.cmd[[echo 1]]
+vim.cmd[[echo 1]]
+vim.cmd[[echo 1]]
+vim.cmd[[echo 1]]
+vim.cmd[[echo 1]]
+vim.cmd[[echo 1]]
+vim.cmd[[echo 1]]
+vim.cmd[[echo 1]]
